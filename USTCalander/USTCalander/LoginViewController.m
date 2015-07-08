3c33d2c08d15241437b853133048d529b201e956
amruthsprasad <mail2amruth88@gmail.com>
2015-07-09 01:59:52 +0530

    Login Page
    Modifications

--- USTCalander/USTCalander/LoginViewController.m
+++ USTCalander/USTCalander/LoginViewController.m
@@ -10,7 +10,7 @@
 #import "AppDelegate.h"
 
 @interface LoginViewController ()
-
+@property (nonatomic,weak)UITextField * selectedTextField;
 @end
 
 @implementation LoginViewController
@@ -54,4 +54,114 @@
 - (IBAction)forgotPasswordAction:(id)sender {
 }
 
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    _selectedTextField=textField;
+    return YES;
+}
+
+
+
+#define kOFFSET_FOR_KEYBOARD 50.0
+
+-(void)keyboardWillShow {
+    // Animate the current view out of the way
+//    if (_selectedTextField==_passwordTextField) {
+        if (self.view.frame.origin.y >= 0)
+        {
+            [self setViewMovedUp:YES];
+        }
+        else if (self.view.frame.origin.y < 0)
+        {
+            [self setViewMovedUp:NO];
+        }
+//    }
+}
+
+-(void)keyboardWillHide {
+//    if (_selectedTextField==_passwordTextField) {
+        if (self.view.frame.origin.y >= 0)
+        {
+            [self setViewMovedUp:YES];
+        }
+        else if (self.view.frame.origin.y < 0)
+        {
+            [self setViewMovedUp:NO];
+        }
+   // }
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)sender
+{
+    if ([sender isEqual:_passwordTextField])
+    {
+        //move the main view, so that the keyboard does not hide it.
+//        if  (self.view.frame.origin.y >= 0)
+//        {
+//            [self setViewMovedUp:YES];
+//        }
+    }
+}
+
+//method to move the view up/down whenever the keyboard is shown/dismissed
+-(void)setViewMovedUp:(BOOL)movedUp
+{
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.3]; // if you want to slide up the view
+    
+    CGRect rect = self.view.frame;
+    if (movedUp)
+    {
+        // 1. move the view's origin up so that the text field that will be hidden come above the keyboard
+        // 2. increase the size of the view so that the area behind the keyboard is covered up.
+        rect.origin.y -= kOFFSET_FOR_KEYBOARD;
+        rect.size.height += kOFFSET_FOR_KEYBOARD;
+    }
+    else
+    {
+        // revert back to the normal state.
+        rect.origin.y += kOFFSET_FOR_KEYBOARD;
+        rect.size.height -= kOFFSET_FOR_KEYBOARD;
+    }
+    self.view.frame = rect;
+    
+    [UIView commitAnimations];
+}
+
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    // register for keyboard notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    // unregister for keyboard notifications while not visible.
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
 @end
