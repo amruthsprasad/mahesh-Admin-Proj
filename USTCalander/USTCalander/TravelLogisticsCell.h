8a5cf55070f29317de67d2eb859c686ca5ad508c
Rony Antony <ronyantony00@gmail.com>
2015-07-22 09:04:00 +0530

    Travel Logistics View

--- USTCalander/USTCalander/TravelLogisticsCell.h
+++ USTCalander/USTCalander/TravelLogisticsCell.h
@@ -1,17 +1,18 @@
 //
-//  AttendeesFeedCell.h
+//  TravelLogisticsCell.h
 //  USTCalander
 //
-//  Created by Rony Antony on 16/07/15.
+//  Created by Rony Antony on 22/07/15.
 //  Copyright (c) 2015 Amruth. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AttendeesFeedCell : UITableViewCell
+@interface TravelLogisticsCell : UITableViewCell
 
 @property (weak, nonatomic) IBOutlet UILabel * titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel * descrip;
+@property (weak, nonatomic) IBOutlet UILabel * subDescrip;
 @property (weak, nonatomic) IBOutlet UIImageView * profileImg;
 
 @end
---
+++ USTCalander/USTCalander/TravelLogisticsView.h
@@ -0,0 +1,15 @@
+//
+//  TravelLogisticsView.h
+//  USTCalander
+//
+//  Created by Rony Antony on 22/07/15.
+//  Copyright (c) 2015 Amruth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ContainerBridgeView.h"
+#import "TravelLogisticsCell.h"
+
+@interface TravelLogisticsView : UIViewController
+
+@end
